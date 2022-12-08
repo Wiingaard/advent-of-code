e09fd663cadb1a005230f9c90b894a5e21c6205a
Martin <martin.wiin@gmail.com>
2022-12-08 20:28:53 +0100

    Day 8

--- 8/Sources/TreetopTreeHouse/TreetopTreeHouse.swift
+++ 8/Sources/TreetopTreeHouse/TreetopTreeHouse.swift
@@ -5,102 +5,62 @@ public struct TreetopTreeHouse {
         case up, down, right, left
     }
     
-    
     public static func main() {
         
         let treeField = input
             .components(separatedBy: "\n")
-            .map { line in
-                return line.map { Int(String($0))! }
-            }
-            
-        let hLength = treeField[0].count
-        let vLength = treeField.count
+            .map { $0.map { Int(String($0))! } }
         
         var visibleTrees = 0
         var highestViewScore = 0
-        
-        func isOnEdge(h: Int, v: Int) -> Bool {
-            h == 0 || v == 0 || h == hLength || v == vLength
-        }
         
         func getDirection(_ direction: Direction, h: Int, v: Int) -> [Int] {
             switch direction {
             case .left:
                 let row = treeField[v]
-                return row.first(to: h)
+                return row.first(to: h).reversed()
             case .right:
                 let row = treeField[v]
                 return row.last(from: h)
             case .up:
                 let col = treeField.map { $0[h] }
-                return col.first(to: v)
+                return col.first(to: v).reversed()
             case .down:
                 let col = treeField.map { $0[h] }
                 return col.last(from: v)
             }
         }
         
-        func viewDistance(_ direction: Direction, h: Int, v: Int, height: Int) -> Int {
-            switch direction {
-            case .left:
-                let row = treeField[v]
-                return row.first(to: h).reversed().viewDirection(height: height)
-            case .right:
-                let row = treeField[v]
-                return row.last(from: h).viewDirection(height: height)
-            case .up:
-                let col = treeField.map { $0[h] }
-                return col.first(to: v).reversed().viewDirection(height: height)
-            case .down:
-                let col = treeField.map { $0[h] }
-                return col.last(from: v).viewDirection(height: height)
-            }
-        }
-        
         treeField.enumerated().forEach { (v, row) in
-            
             row.enumerated().forEach { (h, tree) in
                 func debug(_ message: String) {
                     print("Tree (\(v),\(h) is: '\(message)'")
                 }
                 
-                /* Uncomment for part 1
-                guard !isOnEdge(h: h, v: v) else {
+                // Part 1
+                let leftTrees = getDirection(.left, h: h, v: v)
+                let rightTrees = getDirection(.right, h: h, v: v)
+                let upTrees = getDirection(.up, h: h, v: v)
+                let downTrees = getDirection(.down, h: h, v: v)
+                
+                let leftBlocked = leftTrees.contains { $0 >= tree }
+                let rightBlocked = rightTrees.contains { $0 >= tree }
+                let upBlocked = upTrees.contains { $0 >= tree }
+                let downBlocked = downTrees.contains { $0 >= tree }
+                
+                if !leftBlocked || !rightBlocked || !upBlocked || !downBlocked {
                     visibleTrees += 1
-                    debug("Visible from edge")
-                    return
-                }
-                 */
-                
-                let leftBlocked = getDirection(.left, h: h, v: v).contains { $0 >= tree }
-                let rightBlocked = getDirection(.right, h: h, v: v).contains { $0 >= tree }
-                let upBlocked = getDirection(.up, h: h, v: v).contains { $0 >= tree }
-                let downBlocked = getDirection(.down, h: h, v: v).contains { $0 >= tree }
-                
-                let visible = !leftBlocked || !rightBlocked || !upBlocked || !downBlocked
-                
-                if visible {
-                    // debug("From center: left \(!leftBlocked) right \(!rightBlocked) up \(!upBlocked) down \(!downBlocked)")
-                    visibleTrees += 1
-                } else {
-                    // debug("Not visible")
                 }
                 
-                let leftView = viewDistance(.left, h: h, v: v, height: tree)
-                let rightView = viewDistance(.right, h: h, v: v, height: tree)
-                let upView = viewDistance(.up, h: h, v: v, height: tree)
-                let downView = viewDistance(.down, h: h, v: v, height: tree)
+                // Part 2
+                let leftView = leftTrees.viewDirection(height: tree)
+                let rightView = rightTrees.viewDirection(height: tree)
+                let upView = upTrees.viewDirection(height: tree)
+                let downView = downTrees.viewDirection(height: tree)
                 
                 let viewScore = (leftView * rightView * upView * downView)
                 
-                if (v == 0 && h == 2) {
-                    print("Tree:", tree)
-                    print("break")
-                }
-                
                 if viewScore > highestViewScore {
-                    debug("New high score \(viewScore)! leftView: \(leftView) rightView: \(rightView) upView: \(upView) downView: \(downView)")
                     highestViewScore = viewScore
                 }
             }
@@ -133,4 +93,3 @@ extension Array where Element == Int {
         return distance(from: startIndex, to: index) + 1
     }
 }
-
